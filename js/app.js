1655f8c2e2a17a7b70f8056912276a4241467ae9
John De Leon <112483991+JohnOGama@users.noreply.github.com>
2022-12-28 18:28:54 +0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const hamburger = document.querySelector('.header .navbar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .navbar .nav-list ul');
+const header = document.querySelector('.header .container');
+
+hamburger.addEventListener('click',()=>{
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll',()=>{
+    var scroll_poistion = window.scrollY;
+    if(scroll_poistion > 250){
+        header.style.backgroundColor = '#43cbac';
+    }else{
+        header.style.backgroundColor = 'transparent';
+    }
+});
+
+const scrollToTop = () => {
+    window.scroll({
+        top: 0,
+        behavior: 'smooth',
+    });
+};
+
+document.querySelector("button").onclick = scrollToTop;
